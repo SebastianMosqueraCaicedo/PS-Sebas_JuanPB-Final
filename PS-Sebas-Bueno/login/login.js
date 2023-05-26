aaf60bfc079ea534772671164635ad9bffbc15bf
tabo <1005893565@u.icesi.edu.com>
2023-05-26 07:17:11 -0500

    fix login reloading the page

--- PS-Sebas-Bueno/login/login.js
+++ PS-Sebas-Bueno/login/login.js
@@ -1,19 +1,19 @@
 import { logInUser } from '../firebase.js'
 
 const buttonLogIn = document.querySelector('#form-button')
-buttonLogIn.addEventListener('click', () => logIn())
+buttonLogIn.addEventListener('click', (e) => logIn(e))
 
-async function logIn() {
-    const email = document.getElementById('email-input').value
-    const pass = document.getElementById('pass-input').value
+async function logIn(e) {
+	e.preventDefault();
+	const email = document.getElementById('email-input').value
+	const pass = document.getElementById('pass-input').value
 
 
-    const userCreated = await logInUser(email, pass)
-    if (userCreated.status) {
-        alert('Sesion iniciada, uid: ' + userCreated.info)
-    } else {
-        alert(userCreated.info)
-    }
-
+	const userCreated = await logInUser(email, pass)
+	if (userCreated.status) {
+		alert('Sesion iniciada, uid: ' + userCreated.info)
+	} else {
+		alert(userCreated.info)
+	}
 
 }
