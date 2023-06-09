fb316fed7342a84218134f3b2e9e2457636d1fee
tabo <1005893565@u.icesi.edu.com>
2023-06-08 20:40:19 -0500

    base admin page

---
+++ PS-Sebas-Bueno/admin/admin.js
@@ -0,0 +1,23 @@
+
+const sendButt = document.getElementById('ad-send');
+
+let newItem = {
+	"name": "",
+	"desc": "",
+	"price": 0,
+	"date": "",
+	"cat": "",
+	"col": "",
+	"img": [""]
+}
+
+function checkTypes(){
+}
+
+function checkLength(){
+}
+
+function uploadItem(){
+}
+
+sendButt.AddEventListener('click', uploadItem);
